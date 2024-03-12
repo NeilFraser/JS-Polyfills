643528337df5099f6623f35ca5fc4ab3ff8eae43
Neil Fraser <fraser@google.com>
2024-03-12 12:21:15 +0100

    Add Function.prototype.bind

--- function.js
+++ function.js
@@ -1,10 +1,35 @@
+Object.defineProperty(Function.prototype, 'bind', {
+  configurable: true,
+  writable: true,
+  value: function bind(oThis) {
+    // Polyfill copied from:
+    // developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+    if (typeof this !== 'function') {
+      throw TypeError('What is trying to be bound is not callable');
+    }
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+    if (this.prototype) {
+      fNOP.prototype = this.prototype;
+    }
+    fBound.prototype = new fNOP();
+    return fBound;
+  }
+});
 
 Object.defineProperty(Function.prototype, 'call', {
   configurable: true,
   enumerable: false,
   writable: true,
   value: function(thisArg, var_args) {
-    if (typeof this != 'function') {
+    if (typeof this !== 'function') {
       throw TypeError('this is not a function');
     }
     var args = [];
