323126d37fd5567d21163383fdf947f4d0f6a541
Neil Fraser <fraser@google.com>
2024-03-12 12:20:45 +0100

    Coerce to Number in isNaN and isFinite
    `isNaN({})` was `false`, should be `true`.
    `isFinite({})` was `true`, should be `false`.
    
    Also simplify `parseFloat`, but no functional change.

--- global.js
+++ global.js
@@ -1,25 +1,26 @@
 
 var isNaN = function(testValue) {
+  testValue -= 0;
   return testValue !== testValue;
 };
 
 var isFinite = function(testValue) {
+  testValue -= 0;
   return (testValue === testValue) &&  // NaN
-      testValue != Infinity && testValue != -Infinity;
+      testValue !== Infinity && testValue !== -Infinity;
 };
 
 var parseFloat = function(str) {
-  if (typeof str == 'number') {
+  if (typeof str === 'number') {
     return str;  // NOP
-  } else if (typeof str != 'string') {
-    str += '';
   }
+  str += '';
   var cursor = 0;
   // Trim off left-side whitespace.
   var white = ' \f\n\r\t\v\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004' +
       '\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000' +
       '\ufeff\uFEFF\xA0';
-  while (cursor < str.length && white.indexOf(str[cursor]) != -1) {
+  while (cursor < str.length && white.indexOf(str[cursor]) !== -1) {
     cursor++;
   }
   // Is there a sign?
@@ -30,9 +31,7 @@ var parseFloat = function(str) {
   // Infinity?
   if (str[cursor] == 'I') {
     // Committed to either Infinity or NaN.
-    if (str[++cursor] == 'n' && str[++cursor] == 'f' && str[++cursor] == 'i' &&
-        str[++cursor] == 'n' && str[++cursor] == 'i' && str[++cursor] == 't' &&
-        str[++cursor] == 'y') {
+    if (str.indexOf('Infinity') === cursor) {
       return negative ? -Infinity : Infinity;
     }
     return NaN;
@@ -49,7 +48,7 @@ var parseFloat = function(str) {
   var exp_negative = false;
   while (cursor < str.length) {
     var letter = str[cursor];
-    if (state == 0) {
+    if (state === 0) {
       if (letter >= '0' && letter <= '9') {
         number = number * 10 + (letter - 0);
         ok = true;
@@ -60,7 +59,7 @@ var parseFloat = function(str) {
       } else {
         break;
       }
-    } else if (state == 1) {
+    } else if (state === 1) {
       if (letter >= '0' && letter <= '9') {
         decimal_divider *= 10;
         number += letter / decimal_divider;
@@ -70,7 +69,7 @@ var parseFloat = function(str) {
       } else {
         break;
       }
-    } else if (state == 2) {
+    } else if (state === 2) {
       if (letter == '+') {
       } else if (letter == '-') {
         exp_negative = true;
@@ -80,7 +79,7 @@ var parseFloat = function(str) {
         break;
       }
       state = 3;
-    } else if (state == 3) {
+    } else if (state === 3) {
       if (letter >= '0' && letter <= '9') {
         exp_integer = exp_integer * 10 + (letter - 0);
       } else {
@@ -137,7 +136,7 @@ var parseInt;
     var white = ' \f\n\r\t\v\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004' +
         '\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000' +
         '\ufeff\uFEFF\xA0';
-    while (cursor < str.length && white.indexOf(str[cursor]) != -1) {
+    while (cursor < str.length && white.indexOf(str[cursor]) !== -1) {
       cursor++;
     }
     // Is there a sign?
